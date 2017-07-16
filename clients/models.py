d662d9cc6231a656a2e4cd2aab1e6bf6798b5f8e
tmusters <tmusters@gmail.com>
2017-07-16 14:19:54 -0400

    simple model for clients plus migrations

--- clients/models.py
+++ clients/models.py
@@ -1,6 +1,6 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+
+class Clients(models.Model):
+    first_name = models.CharField(max_length=30),
+    last_name = models.CharField(max_length=30)
