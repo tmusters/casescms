3ffea8eeb388f3639aa5f57ff83bc0c80b5e1579
tmusters <tmusters@gmail.com>
2017-07-16 16:13:32 -0400

    added basic admin console and added clients to it. Also requirements.txt

--- clients/models.py
+++ clients/models.py
@@ -2,5 +2,9 @@ from django.db import models
 
 
 class Client(models.Model):
-    first_name = models.CharField(max_length=30),
-    last_name = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=30, default="", blank=False)
+    last_name = models.CharField(max_length=30, default="", blank=False)
+    company = models.CharField(max_length=50, default="None", blank=False)
+
+    def __str__(self):
+        return format(self.last_name) + ", " + format(self.first_name)
